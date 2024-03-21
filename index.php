45092434cec8701a02ef6baeaeb94a876eabb34d
DogeBloxy <supertiago.260@gmail.com>
2024-03-21 11:36:34 +0100

    Ajustement

--- index.php
+++ index.php
@@ -31,6 +31,6 @@ if (strlen($password) < 8) {
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="alert alert-<?php echo ($error == true) ? 'danger' : 'success'; ?>" role="alert"><?php echo $message ?></div>
+<div class="alert alert-<?php echo ($error) ? 'danger' : 'success'; ?>" role="alert"><?php echo $message ?></div>
 </body>
 </html>
